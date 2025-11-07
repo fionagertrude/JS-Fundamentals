29372ac7d7c6fbc381b8128eebb0592642dc3e12
fionagertrude <fionagertrude.k@gmail.com>
2025-11-07 14:27:36 +0300

    commit 3

--- 2-arguments.js
+++ 2-arguments.js
@@ -7,7 +7,10 @@ if (args.length === 0) {
 } else {
     console.log(`Two arguments provided: ${args[0]} and ${args[1]}`);
 }
-// Example Outputs:
-// No arguments provided.
-// One argument provided: arg1
-// Two arguments provided: arg1 and arg2
+//to run it
+// node 2-arguments.js 
+// output: No argument provided.
+// node 2-arguments.js first 
+// output: One Argument provided: first
+// node 2-arguments.js second 
+// output: Two arguments provided: second and third
---
+++ README.md
@@ -0,0 +1,16 @@
+task0
+// Output:
+// JavaScript is amazing
+task1
+// Output:
+// C is fun
+// Python is cool
+// JavaScript is amazing
+task2
+//to run it
+// node 2-arguments.js 
+// output: No argument provided.
+// node 2-arguments.js first 
+// output: One Argument provided: first
+// node 2-arguments.js second 
+// output: Two arguments provided: second and third
