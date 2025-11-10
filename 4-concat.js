3aa3cdfebf297d39914b18e74afdb9b6f7f45919
fionagertrude <fionagertrude.k@gmail.com>
2025-11-10 20:43:24 +0300

    second commit

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,7 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-console.log(arg1 + arg2);
+console.log(`${arg1} is ${arg2}`);
 // To run it:
 // node 4-concat.js Hello World
-// output: HelloWorld
+// output: Hello is World
